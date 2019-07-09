5a8f17a330368b842ce17ffbb3055dfed1158bf7
John Wong <yeukhon.tech@gmail.com>
2019-07-08 23:59:52 -0400

    Minecraft DO droplet

---
+++ minecraft/server.tf
@@ -0,0 +1,20 @@
+resource "digitalocean_tag" "droplet_name_tag" {
+  name = "minecraft"
+}
+
+resource "digitalocean_ssh_key" "minecraft_ssh_key" {
+  name       = "minecraft_ssh_key"
+  public_key = "${file("${path.cwd}/data_store/server.pub")}"
+}
+
+resource "digitalocean_droplet" "minecraft" {
+  image     = "ubuntu-18-04-x64"
+  name      = "minecraft"
+  region    = "nyc3"
+  size      = "s-1vcpu-1gb"
+  ssh_keys  = ["${digitalocean_ssh_key.minecraft_ssh_key.id}"]
+  backups   = true
+  tags   = [
+    "${digitalocean_tag.droplet_name_tag.name}",
+  ]
+}
