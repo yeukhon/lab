383fc4cb059f95c65f4dca8f7539e48cb8e65341
John Wong <yeukhon.tech@gmail.com>
2019-07-09 02:26:31 -0400

    Update IP

--- minecraft/server.tf
+++ minecraft/server.tf
@@ -11,7 +11,7 @@ resource "digitalocean_droplet" "minecraft" {
   image     = "ubuntu-18-04-x64"
   name      = "minecraft"
   region    = "nyc3"
-  size      = "s-1vcpu-1gb"
+  size      = "s-2vcpu-2gb"
   ssh_keys  = ["${digitalocean_ssh_key.minecraft_ssh_key.id}"]
   backups   = true
   tags   = [
